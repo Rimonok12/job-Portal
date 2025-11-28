e4014bd8ce0a99b04a757700b9c62cee11f25874
RIMON DEBNATH <68506201+Rimonok12@users.noreply.github.com>
2025-11-28 23:09:11 +0530

    Update requests.js

--- job-portal-frontend/src/api/requests.js
+++ job-portal-frontend/src/api/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5050',
+  baseURL: 'https://job-portal-1-d4oh.onrender.com',
 });
 
 // Attach token to every request
